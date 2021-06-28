497141588f31e1aa15c198894fa79272d41b2240
iben12 <iben@iben.hu>
2021-06-28 14:34:33 +0200

    feat(nodejs): add SIGTERM handling
    TOOL-1095

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -15,3 +15,12 @@ fastify.listen(3000, '0.0.0.0', function (err, address) {
   }
   fastify.log.info(`server listening on ${address}`)
 })
+
+// Handle shutdown (close server, close database connection, wait for ongoing requests to finish, etc)
+process.on("SIGTERM", () => {
+  fastify.log.info("Shutting down gracefully...");
+  fastify.close().then(() => {
+    console.log("Server shut down successfully");
+    process.exit(0);
+  });
+});
